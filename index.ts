c2381dc5c714e28399da871d2d8a4e015fdc5162
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-08-31 23:07:39 +0200

    aa

--- index.ts
+++ index.ts
@@ -9,38 +9,51 @@ const quotes = [
   {
     id: 1,
     quote: 'Either write something worth reading or do something worth writing',
-    quoteName: 'Benjamin Franklin'
+    quoteName: 'Benjamin Franklin',
+    age: 43,
+    image:
+      'https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU4MDk5NzU4ODk0ODE5MDk3/franklin.jpg'
   },
   {
     id: 2,
     quote: 'The best way to predict the future is to create it',
-    quoteName: 'Abraham Lincoln'
+    quoteName: 'Abraham Lincoln',
+    age: 57,
+    image:
+      'https://www.whitehouse.gov/wp-content/uploads/2021/01/16_abraham_lincoln.jpg'
   },
   {
     id: 3,
     quote: 'That which does not kill us makes us stronger.',
-    quoteName: 'Friedrich Nietzsche'
+    quoteName: 'Friedrich Nietzsche',
+    age: 54,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/1/1b/Nietzsche187a.jpg'
   },
   {
     id: 4,
     quote: 'When the going gets tough, the tough get going.',
-    quoteName: 'Joe Kennedy'
+    quoteName: 'Joe Kennedy',
+    age: 63,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/6/6d/Joseph_P._Kennedy%2C_Sr._1938.jpg'
   },
   {
     id: 5,
     quote: 'You only live once, but if you do it right, once is enough.',
-    quoteName: 'Mae West'
+    quoteName: 'Mae West',
+    age: 60,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/f/fd/Mae_West_LAT.jpg'
   },
   {
     id: 6,
     quote: 'The best revenge is massive success.',
-    quoteName: 'Frank Sinatra'
-  },
-  {
-    id: 7,
-    quote: 'Life is like a box of chocolates. You never know what you’re going to get.',
-    quoteName: 'Forrest Gump'
-  },
+    quoteName: 'Frank Sinatra',
+    age: 72,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Frank_Sinatra_%281957_studio_portrait_close-up%29.jpg/1200px-Frank_Sinatra_%281957_studio_portrait_close-up%29.jpg'
+  }
 ]
 
 app.get('/', (req, res) => {
@@ -48,14 +61,28 @@ app.get('/', (req, res) => {
   <h1>Hello World</h1>
   <a href="/quotes">Quotes</a>
   `)
-  
+})
+
+app.get('/quotes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const match = quotes.find(quote => quote.id === id)
+
+  if (!match) {
+    res.status(404).send({ error: 'Not found' })
+  } else {
+    res.send(match)
+  }
 })
 
 app.get('/quotes', (req, res) => {
-    res.send(quotes)
-  })
-  
+  res.send(quotes)
+})
 
 app.listen(port, () =>
   console.log(`Example app listening on port http://localhost:${port}/`)
 )
+
+app.get(`/randomQuote`, (req, res) => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+  res.send(randomQuote)
+})
