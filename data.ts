3f5b98ae48e20062f6f1fa7c4e88fad664a3ac1d
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-09-01 23:42:07 +0200

    added quote author

---
+++ data.ts
@@ -0,0 +1,77 @@
+export const quotes = [
+  {
+    id: 1,
+    quote: 'Either write something worth reading or do something worth writing',
+    authorId : 1
+  },
+  {
+    id: 2,
+    quote: 'The best way to predict the future is to create it',
+    authorId : 2
+  },
+  {
+    id: 3,
+    quote: 'That which does not kill us makes us stronger.',
+    authorId : 3
+  },
+  {
+    id: 4,
+    quote: 'When the going gets tough, the tough get going.',
+    authorId : 4
+  },
+  {
+    id: 5,
+    quote: 'You only live once, but if you do it right, once is enough.',
+    authorId : 5
+  },
+  {
+    id: 6,
+    quote: 'The best revenge is massive success.',
+    authorId : 6
+  }
+]
+
+export const quotesAuthor = [
+  {
+    id: 1,
+    authorName: 'Benjamin Franklin',
+    authorAge: 43,
+    image:
+      'https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU4MDk5NzU4ODk0ODE5MDk3/franklin.jpg'
+  },
+  {
+    id: 2,
+    authorName: 'Abraham Lincoln',
+    authorAge: 57,
+    image:
+      'https://www.whitehouse.gov/wp-content/uploads/2021/01/16_abraham_lincoln.jpg'
+  },
+  {
+    id: 3,
+    authorName: 'Friedrich Nietzsche',
+    authorAge: 54,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/1/1b/Nietzsche187a.jpg'
+  },
+  {
+    id: 4,
+    authorName: 'Joe Kennedy',
+    authorAge: 63,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/6/6d/Joseph_P._Kennedy%2C_Sr._1938.jpg'
+  },
+  {
+    id: 5,
+    authorName: 'Mae West',
+    authorAge: 60,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/f/fd/Mae_West_LAT.jpg'
+  },
+  {
+    id: 6,
+    authorName: 'Frank Sinatra',
+    authorAge: 72,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Frank_Sinatra_%281957_studio_portrait_close-up%29.jpg/1200px-Frank_Sinatra_%281957_studio_portrait_close-up%29.jpg'
+  }
+]
--- index.ts
+++ index.ts
@@ -1,60 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import { quotes} from './data'
+import { quotesAuthor } from './data'
 
 const app = express()
 app.use(cors())
 const port = 2345
-
-const quotes = [
-  {
-    id: 1,
-    quote: 'Either write something worth reading or do something worth writing',
-    quoteName: 'Benjamin Franklin',
-    age: 43,
-    image:
-      'https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU4MDk5NzU4ODk0ODE5MDk3/franklin.jpg'
-  },
-  {
-    id: 2,
-    quote: 'The best way to predict the future is to create it',
-    quoteName: 'Abraham Lincoln',
-    age: 57,
-    image:
-      'https://www.whitehouse.gov/wp-content/uploads/2021/01/16_abraham_lincoln.jpg'
-  },
-  {
-    id: 3,
-    quote: 'That which does not kill us makes us stronger.',
-    quoteName: 'Friedrich Nietzsche',
-    age: 54,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/1/1b/Nietzsche187a.jpg'
-  },
-  {
-    id: 4,
-    quote: 'When the going gets tough, the tough get going.',
-    quoteName: 'Joe Kennedy',
-    age: 63,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/6/6d/Joseph_P._Kennedy%2C_Sr._1938.jpg'
-  },
-  {
-    id: 5,
-    quote: 'You only live once, but if you do it right, once is enough.',
-    quoteName: 'Mae West',
-    age: 60,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/f/fd/Mae_West_LAT.jpg'
-  },
-  {
-    id: 6,
-    quote: 'The best revenge is massive success.',
-    quoteName: 'Frank Sinatra',
-    age: 72,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Frank_Sinatra_%281957_studio_portrait_close-up%29.jpg/1200px-Frank_Sinatra_%281957_studio_portrait_close-up%29.jpg'
-  }
-]
 
 app.get('/', (req, res) => {
   res.send(`
@@ -75,7 +26,11 @@ app.get('/quotes/:id', (req, res) => {
 })
 
 app.get('/quotes', (req, res) => {
-  res.send(quotes)
+  let quotesToSend = quotes.map(quote => {
+    let quoteAuthor = quotesAuthor.find(author => author.id === quote.authorId) 
+    return { ...quote , quoteAuthor }
+  })
+  res.send(quotesToSend)
 })
 
 app.listen(port, () =>
